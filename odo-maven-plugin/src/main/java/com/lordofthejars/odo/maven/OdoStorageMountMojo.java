e64066a9cc182c0cd3fdf7a2bd8b1cf634274c4d
Devang Gaur <dgaur@redhat.com>
2019-03-08 04:55:32 +0530

    Mount storage maven plugin

---
+++ odo-maven-plugin/src/main/java/com/lordofthejars/odo/maven/OdoStorageMountMojo.java
@@ -0,0 +1,49 @@
+package com.lordofthejars.odo.maven;
+
+import com.lordofthejars.odo.core.Odo;
+import com.lordofthejars.odo.core.commands.StorageMountCommand;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+
+import java.util.Map;
+import java.util.logging.Logger;
+
+import static com.lordofthejars.odo.maven.ConfigurationInjector.injectFields;
+
+@Mojo(name="mount-storage")
+public class OdoStorageMountMojo extends AbstractMojo {
+
+    protected Odo odo = null;
+
+    private Logger logger = Logger.getLogger(this.getClass().getName());
+
+    // Current maven project
+    @Parameter(defaultValue= "${project}", readonly = true)
+    protected MavenProject project;
+
+    @Parameter
+    protected Map<String, String> mountStorage;
+
+    @Parameter
+    protected String storageName;
+
+    @Parameter
+    protected String path;
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        if(odo == null) {
+            odo = new Odo();
+        }
+        StorageMountCommand storageMountCommand = odo.mountStorage(storageName)
+                .withPath(project.getBasedir().getAbsolutePath().concat(path)).build();
+
+        injectFields(storageMountCommand, mountStorage, logger);
+        storageMountCommand.execute();
+    }
+
+}
