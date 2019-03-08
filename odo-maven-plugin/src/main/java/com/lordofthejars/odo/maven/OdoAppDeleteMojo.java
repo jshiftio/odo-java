2762e1fde3531e45cc09f1539e78d2c077694f84
Rohan Kumar <rohaan@redhat.com>
2019-03-08 14:58:03 +0530

    Delete App Maven plugin (#27)

---
+++ odo-maven-plugin/src/main/java/com/lordofthejars/odo/maven/OdoAppDeleteMojo.java
@@ -0,0 +1,44 @@
+package com.lordofthejars.odo.maven;
+
+import com.lordofthejars.odo.core.Odo;
+import com.lordofthejars.odo.core.commands.AppDeleteCommand;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+
+import java.util.Map;
+import java.util.logging.Logger;
+
+import static com.lordofthejars.odo.maven.ConfigurationInjector.injectFields;
+
+@Mojo(name = "delete-app")
+public class OdoAppDeleteMojo extends AbstractMojo {
+
+    protected Odo odo = null;
+
+    private Logger logger = Logger.getLogger(this.getClass().getName());
+
+    // Current maven project
+    @Parameter(defaultValue = "${project}", readonly = true)
+    protected MavenProject project;
+
+    @Parameter
+    protected Map<String, String> deleteApp;
+
+    @Parameter
+    protected String appName;
+
+    @Override
+    public void execute() {
+        if (odo == null) {
+            odo = new Odo();
+        }
+        AppDeleteCommand appDeleteCommand = odo.deleteApp(appName)
+                .withProject(project.getBasedir().getAbsolutePath())
+                .build();
+
+        injectFields(appDeleteCommand, deleteApp, logger);
+        appDeleteCommand.execute();
+    }
+}
---
+++ odo-maven-plugin/src/test/java/com/lordofthejars/odo/maven/OdoAppDeleteMojoTest.java
@@ -0,0 +1,41 @@
+package com.lordofthejars.odo.maven;
+
+import com.lordofthejars.odo.core.Odo;
+import com.lordofthejars.odo.testbed.junit5.OdoExecutorStubInjector;
+import com.lordofthejars.odo.testbed.odo.OdoExecutorStub;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.project.MavenProject;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.File;
+
+import static com.lordofthejars.odo.testbed.assertj.OdoExecutorAssertion.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith({MockitoExtension.class, OdoExecutorStubInjector.class})
+public class OdoAppDeleteMojoTest {
+    @Mock
+    MavenProject project;
+
+    @Test
+    public void testMojoBehavior(OdoExecutorStub odoExecutorStub) throws MojoExecutionException, MojoFailureException {
+        // Given
+        OdoAppDeleteMojo odoAppDeleteMojo = new OdoAppDeleteMojo();
+        Odo odo = new Odo(odoExecutorStub);
+
+        when(project.getBasedir()).thenReturn(new File("/tmp/foodir"));
+        odoAppDeleteMojo.project = project;
+        odoAppDeleteMojo.appName = "myapp";
+        odoAppDeleteMojo.odo = odo;
+
+        // When:
+        odoAppDeleteMojo.execute();
+
+        // Then:
+        assertThat(odoExecutorStub).hasExecuted("odo app delete myapp --project /tmp/foodir --force");
+    }
+}
